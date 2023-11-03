8fb03aec9ced068751ad53380f2d07ef70771819
ww <aquawill@msn.com>
2023-11-03 15:32:58 +0800

    Adding json schema files.

--- hdlm_coord_converter.py
+++ hdlm_coord_converter.py
@@ -34,5 +34,5 @@ def hdlm_coord_to_WGS84(b64_signed_integer):
     lng_float_from_int = lng_int_from_bin * (360 / math.pow(2, 32))
     return {'lat': lat_float_from_int, 'lng': lng_float_from_int}
 
-# print(wgs84_to_hdlm_coord(-33.86663, 151.20578))
-# print(hdlm_coord_to_WGS84(4354955124161939766))
+# print(wgs84_to_hdlm_coord(52.283935546875, 12.908935546875))
+# print(hdlm_coord_to_WGS84(604341051469070336))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import os
-from google.protobuf.json_format import MessageToJson
 
+from google.protobuf.json_format import MessageToJson
 from here.platform import Platform
 from here.platform import adapter
 
@@ -17,28 +17,34 @@ env = platform.environment
 config = platform.platform_config
 status = platform.get_status()
 print(status)
-hrn = 'hrn:here:data::olp-here-had:here-hdlm-protobuf-weu-2'
-catalog = platform.get_catalog(hrn=hrn)
+hrn_hdlm = 'hrn:here:data::olp-here-had:here-hdlm-protobuf-weu-2'
+hrn_rib_2 = 'hrn:here:data::olp-here:rib-2'
+catalog = platform.get_catalog(hrn=hrn_hdlm)
 catalog_detail = json.loads(json.dumps(catalog.get_details()))
 
-partition_ids = [377893071]
+hmc_partition_ids = [23618402, 23618403]
+hdlm_tile_ids = [377893071]
 
 layers = catalog_detail['layers']
 for layer in layers:
-    if layer['partitioningScheme'] == 'heretile' and layer['layerType'] == 'versioned':
-        print(layer['id'])
+    print(layer['id'], layer['partitioningScheme'], layer['layerType'])
+    if layer['partitioningScheme'] == 'heretile':
+        # print(layer['id'])
         versioned_layer = catalog.get_layer(layer['id'])
-        partitions = versioned_layer.read_partitions(partition_ids=partition_ids)
+        partitions = versioned_layer.read_partitions(partition_ids=hdlm_tile_ids)
         for p in partitions:
             versioned_partition, partition_content = p
             print(f"{versioned_partition.id}: {type(partition_content)}")
             decoded_content = adapter.DecodedMessage(partition_content)
             decoded_content_json = MessageToJson(decoded_content)
-            if not os.path.exists('decoded_tile'):
-                os.mkdir('decoded_tile')
-            if not os.path.exists(os.path.join('decoded_tile', str(versioned_partition.id))):
-                os.mkdir(os.path.join('decoded_tile', str(versioned_partition.id)))
-            with open(os.path.join('decoded_tile', str(versioned_partition.id),
+            hrn_folder_name = catalog.hrn.replace(':','_')
+            if not os.path.exists('decoded'):
+                os.mkdir('decoded')
+            if not os.path.exists(os.path.join('decoded', hrn_folder_name)):
+                os.mkdir(os.path.join('decoded', hrn_folder_name))
+            if not os.path.exists(os.path.join('decoded', hrn_folder_name, str(versioned_partition.id))):
+                os.mkdir(os.path.join('decoded', hrn_folder_name, str(versioned_partition.id)))
+            with open(os.path.join('decoded', hrn_folder_name, str(versioned_partition.id),
                                    f"{versioned_partition.id}_{layer['id']}.json"), mode='w',
                       encoding='utf-8') as output:
                 output.write(decoded_content_json)
